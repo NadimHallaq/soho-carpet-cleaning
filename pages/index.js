479b030fd9d7f228f873940089294cd00d871619
nadim hallaq <nadimhallaq@PillarBase-Inc.local>
2021-04-20 08:20:07 -0400

    second commit

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,17 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Services from "../components/Services";
 import Commercial from "../components/commercial";
+import ChooseUs from "../components/ChooseUs";
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Soho Carpet Cleaning</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
       <Hero />
+      <ChooseUs />
       <Services />
       <Commercial />
     </div>
